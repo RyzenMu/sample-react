39555cb4b54d0af35728c59f60edc0cfac621d33
RyenMu <muruga13in2022@gmail.com>
2023-01-30 07:12:39 +0530

    first commit

---
+++ react.js
@@ -0,0 +1,25 @@
+class App extends React.Component {
+    state = {
+        btn_color : 'button-red',
+    }
+
+
+    onClick = (event) => {
+        this.setState( {
+            btn_color: "button-grey"
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <button onClick={this.onClick} className={this.state.btn_color}> click da </button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
